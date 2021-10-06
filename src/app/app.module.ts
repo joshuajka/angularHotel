e13e0fd8fca697878afeb72292cf6a4cf6e4035b
Joshua Abbate <55669412+joshuajka@users.noreply.github.com>
2021-10-05 23:01:36 -0300

    version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './component/template/header/header.component';
+import { FooterComponent } from './component/template/footer/footer.component';
+import { SidenavComponent } from './component/template/sidenav/sidenav.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { HotelListComponent } from './component/view/hotel/hotel-list/hotel-list.component';
+import { HospedagemListComponent } from './component/view/hospedagem/hospedagem-list/hospedagem-list.component';
+import { HospedeListComponent } from './component/view/hospede/hospede-list/hospede-list.component';
+import { HomeComponent } from './component/view/home/home.component';
+import { QuartoListComponent } from './component/view/quarto/quarto-list/quarto-list.component';
+import { QuartoFormComponent } from './component/view/quarto/quarto-form/quarto-form.component';
+import { HospedeFormComponent } from './component/view/hospede/hospede-form/hospede-form.component';
+import { HospedagemFormComponent } from './component/view/hospedagem/hospedagem-form/hospedagem-form.component';
+import { HotelFormComponent } from './component/view/hotel/hotel-form/hotel-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    HotelListComponent,
+    HospedagemListComponent,
+    HospedeListComponent,
+    HomeComponent,
+    QuartoListComponent,
+    QuartoFormComponent,
+    HospedeFormComponent,
+    HospedagemFormComponent,
+    HotelFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
